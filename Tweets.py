558febeb9e5771f6dbd573f5f3c439b7399356cc
Mohit Kumar <kmohit0002@gmail.com>
2024-12-05 00:12:02 +0530

    update tweet fetching logic

--- Tweets.py
+++ Tweets.py
@@ -33,7 +33,7 @@ class FetchTweets:
         url = "https://twitter154.p.rapidapi.com/user/tweets"
         querystring = {"username":username,"limit":"100","include_replies":"false","include_pinned":"true"}
         headers = {
-            "x-rapidapi-key": "39c02cdab7msh7424bcdcee10cc5p19b56ajsn06ca92d14230",
+            "x-rapidapi-key": os.getenv('API_KEY'),
             "x-rapidapi-host": "twitter154.p.rapidapi.com"
         }
         response = requests.get(url, headers=headers, params=querystring)
